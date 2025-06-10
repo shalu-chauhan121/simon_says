075b760caef18492b7ed8ba101ea3ddf1b313556
Shalu Chauhan <shalu859582@gmail.com>
2025-06-10 13:13:32 +0530

    updated

--- app.js
+++ app.js
@@ -1,21 +1,68 @@
-btn=document.querySelector(".add");
-inp=document.querySelector('input');
-l=document.querySelector('ul');
-btn.addEventListener("click",function(){
-    let item=document.createElement('li');
-    item.innerText=inp.value;
-    let b=document.createElement('button');
-    b.innerText="Delete";
-    b.classList.add("delete");
-    item.appendChild(b);
-    l.appendChild(item);
-    inp.value="";
-});
 
-l.addEventListener("click",function(event){
-//  console.log(event.target.nodeName);
-  if(event.target.nodeName==="BUTTON"){
-    let listItem=event.target.parentElement;
-    listItem.remove();
-  }
+let gameseq=[];
+let userseq=[];
+let level=0;
+let started=false;
+lvl=document.querySelector('.level');
+document.addEventListener("keypress",function(){
+     if(started==false) {
+        started=true; 
+        levelUp();
+     }
 })
+let btns=["pink","skyblue","orange","blue"];
+function gameflash(btn){
+    btn.classList.add("flash");
+    setTimeout(() => {
+    btn.classList.remove("flash");
+    },250);
+}
+function userflash(btn){
+    btn.classList.add("userflash");
+    setTimeout(function(){
+       btn.classList.remove("userflash");
+    },250);
+   }
+function levelUp(){
+    userseq=[];
+    level++;
+    lvl.innerText=`Level ${level}`;
+    let rdm=Math.floor(Math.random()*4);
+    let cls=btns[rdm];
+    let btn=document.querySelector(`.${cls}`);
+    gameseq.push(cls);  
+    gameflash(btn);
+}
+function checkbtn(idx){
+   if(userseq[idx]===gameseq[idx]){
+    if(userseq.length==gameseq.length){
+       setTimeout(levelUp,500);
+    }
+   }
+   else {
+    lvl.innerHTML=`Game over!! your score was <b> ${level*10} </b> </br> Press any key to restart `
+    document.querySelector("body").style.backgroundColor="red";
+    setTimeout(function(){
+        document.querySelector("body").style.backgroundColor="white";
+    },200);
+    reset();
+   }
+}
+function btnpress(){
+    let btn=this;
+    userflash(btn);
+    let color=btn.getAttribute("id");
+    // console.log(color);
+    userseq.push(color);
+    checkbtn(userseq.length-1);
+}
+let allbtn=document.querySelectorAll(".btn");
+for(let btn of allbtn){
+    btn.addEventListener("click",btnpress);
+}
+function reset(){
+    started=false;
+    gameseq=[];
+    userseq=[];
+    level=0;
+}
